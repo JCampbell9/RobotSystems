3b97ed66528d237cf4cc6327a02d751603ae3f08
JCampbell9 <campbjos@oregonstate.edu>
2021-04-06 12:31:36 -0700

    calibration

--- test_steering_cali.py
+++ test_steering_cali.py
@@ -1,13 +1,13 @@
 from picarx_improved import *
 import time
 
-set_dir_servo_angle(0)
+set_dir_servo_angle(18)
+time.sleep(.5)
 
-forward(50)
-
+forward(60)
 print("going forward...")
 
-time.sleep(5)
+time.sleep(3)
 
 print("stopping...")
 stop()
