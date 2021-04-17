d9b9e31a2ab55ce64ce7025592770ef1e2464c21
JCampbell9 <campbjos@oregonstate.edu>
2021-04-17 14:56:57 -0700

    Update workspace.xml and test_camera.py

--- updated_control/test_camera.py
+++ updated_control/test_camera.py
@@ -7,7 +7,13 @@ rawCapture = PiRGBArray(cam.camera)
 cam.camera.capture(rawCapture, format="bgr")
 image = rawCapture.array
 
-cv2.imwrite('test_image.jpg',image)
+cv2.imwrite('test_image.jpg', image)
+frame = cv2.imread(image)
+
+cv2.imwrite('test2_image.jpg', frame)
+
+hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
 
 #cv2.imshow("Image", image)
 
