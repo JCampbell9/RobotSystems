6ab36fd5dbb960acc80580b40b3d2f343cfcc0e1
JCampbell9 <campbjos@oregonstate.edu>
2021-04-17 15:04:37 -0700

    testing

--- updated_control/test_camera.py
+++ updated_control/test_camera.py
@@ -1,6 +1,7 @@
 from ezblock import camera
 from picamera.array import PiRGBArray
 import cv2
+import numpy as np
 
 cam = camera.Camera()
 rawCapture = PiRGBArray(cam.camera)
@@ -8,12 +9,15 @@ cam.camera.capture(rawCapture, format="bgr")
 image = rawCapture.array
 
 cv2.imwrite('test_image.jpg', image)
-frame = cv2.imread(image)
+#frame = cv2.imread(image)
 
-cv2.imwrite('test2_image.jpg', frame)
+#cv2.imwrite('test2_image.jpg', frame)
 
-hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+lower_blue = np.array([60, 40, 40])
+upper_blue = np.array([150, 255, 255])
+mask = cv2.inRange(hsv, lower_blue, upper_blue)
+cv2.imwrite('testing_image2.jpg', mask)
 
 #cv2.imshow("Image", image)
 
